7bf00cf5de476385c5188945c4759be8d543d506
Rand al'Thor <RandyTaschner@Randels-MacBook-Pro.local>
2017-12-11 08:57:05 -0700

    fixed jade files, gallery stage open in effect, added about section, starting gallery thumb area

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,10 +1,17 @@
-$(function() {
-	galleryBelt();
+
+// Smooth Scrolling
+$(document).ready(function coolScroll() {
+	
+	$("#gallery-link").click(function() {
+		$path=$("#gallery").offset().top;
+			$('body').animate({scrollTop:$path},1000);
+	});
+
+	console.log('coolScroll');
 });
 
-
-
-function galleryBelt() {
-
-	$('.')
-}
+$(function(){
+  $('.stage-toggle').on('click', function() {
+     $('.gallery-stage-container').toggleClass('stage-open');
+   });
+});
